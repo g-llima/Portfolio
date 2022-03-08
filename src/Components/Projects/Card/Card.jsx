86bed26bc2830b0650dd65cf2424e1e165cc15e3
Gabriel Lima <gabrielrtxon@gmail.com>
2022-03-08 11:09:42 -0300

    Project card props

--- src/Components/Projects/Card/Card.jsx
+++ src/Components/Projects/Card/Card.jsx
@@ -2,43 +2,33 @@ import React from "react";
 
 import "./CSS/Card.css";
 
-function Card() {
+function Card({ title, description, topics, img, github, website }) {
   return (
     <div className="card">
       <div className="card__content">
         <div className="card__content__front">
-          <img
-            className="card__content__front__img"
-            src="https://files.tecnoblog.net/meiobit/wp-content/uploads/2021/09/lost-in-random-001.jpg"
-            alt="a"
-          />
-          <h3 className="card__content__front__title">Lorem ipsum dolor</h3>
-          <p className="card__content__front__desc">
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-          </p>
+          <img className="card__content__front__img" src={img} alt={title} />
+          <h3 className="card__content__front__title">{title}</h3>
+          <p className="card__content__front__desc">{description}</p>
           <div className="card__content__front__topics">
             <ul className="card__content__front__topics__list">
-              <li className="card__content__front__topics__list__item HTML__CARD">
-                HTML
-              </li>
-              <li className="card__content__front__topics__list__item CSS__CARD">
-                CSS
-              </li>
-              <li className="card__content__front__topics__list__item REACT__CARD">
-                REACT
-              </li>
-              <li className="card__content__front__topics__list__item JavaScript__CARD">
-                JavaScript
-              </li>
+              {topics.map((item, index) => (
+                <li
+                  key={index}
+                  className={`card__content__front__topics__list__item ${item}__CARD`}
+                >
+                  {item}
+                </li>
+              ))}
             </ul>
           </div>
         </div>
         <div className="card__content__back">
           <div className="card__content__back__icons">
-            <a href="#" target="__blank">
+            <a href={website} target="__blank">
               <i className="fas fa-external-link"></i>
             </a>
-            <a href="https://github.com/g-llima" target="__blank">
+            <a href={github} target="__blank">
               <i className="fab fa-github"></i>
             </a>
           </div>
--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -3,13 +3,50 @@ import "./CSS/Projects.css";
 
 import Card from "./Card/Card";
 
+const cards = [
+  {
+    title: "Lorem ipsum dolor",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    topics: ["HTML", "CSS", "REACT", "JavaScript"],
+    img: "https://files.tecnoblog.net/meiobit/wp-content/uploads/2021/09/lost-in-random-001.jpg",
+    github: "https://github.com/g-llima",
+    website: "#",
+  },
+  {
+    title: "Lorem ipsum dolor",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    topics: ["HTML", "CSS", "JavaScript"],
+    img: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/83/Android_TV_game_controller.jpg/1200px-Android_TV_game_controller.jpg",
+    github: "https://github.com/g-llima",
+    website: "#",
+  },
+  {
+    title: "Lorem ipsum dolor",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit.",
+    topics: ["HTML", "CSS", "REACT", "JavaScript"],
+    img: "https://cdnportal.mobalytics.gg/production/2021/06/64779e0a-league-of-legends-game-bg.png",
+    github: "https://github.com/g-llima",
+    website: "#",
+  },
+];
+
 function Projects() {
   return (
     <div className="projects">
       <div className="projects__content">
         <h2 className="projects__content__title">Projetos</h2>
         <div className="projects__content__items">
-          <Card />
+          {cards.map((item, index) => (
+            <Card
+              key={index}
+              title={item.title}
+              description={item.description}
+              topics={item.topics}
+              img={item.img}
+              github={item.github}
+              website={item.website}
+            />
+          ))}
         </div>
       </div>
     </div>
