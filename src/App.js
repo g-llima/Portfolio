e6267ad82226f295d2f478e2468eeb52779cff49
Gabriel Lima <gabrielrtxon@gmail.com>
2022-03-09 09:50:04 -0300

    Removed cursor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import "./GLOBAL CSS/App.css";
 
 import Home from "./Pages/Home/Home";
-import Cursor from "./Components/Custom Cursor/Cursor";
 
 function App() {
   return (
     <div className="App">
-      <Cursor />
       <Home />
     </div>
   );
